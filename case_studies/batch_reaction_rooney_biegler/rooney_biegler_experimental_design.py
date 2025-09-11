c27fa99cef4f6422f5e9c369bd89a2b0707e3560
Daniel Laky <29078718+djlaky@users.noreply.github.com>
2025-09-11 15:04:24 -0400

    Found the bug. Cleaning up

--- case_studies/batch_reaction_rooney_biegler/rooney_biegler_experimental_design.py
+++ case_studies/batch_reaction_rooney_biegler/rooney_biegler_experimental_design.py
@@ -60,17 +60,17 @@ def run_greybox_optimization():
         FIM_prior[1][1] += 1e-6
 
     # Compute new FIM calculation for a range of time values
-    objective_options = ["determinant"]
-    # "trace",
-    # "minimum_eigenvalue",
-    # "condition_number"]
+    objective_options = ["determinant",
+                         "trace",
+                         "minimum_eigenvalue",
+                         "condition_number"]
 
     optimal_points = []
     optimal_objective_value = 0
 
     # Define our own grey box solver
     grey_box_solver = pyo.SolverFactory("cyipopt")
-    # grey_box_solver.config.options["hessian_approximation"] = "limited-memory"
+    grey_box_solver.config.options["hessian_approximation"] = "limited-memory"
     grey_box_solver.config.options["linear_solver"] = "ma27"
     grey_box_solver.config.options['mu_strategy'] = "monotone"
 
@@ -132,92 +132,8 @@ def run_greybox_optimization():
                 float(doe_obj_nongb.results["Experiment Design"][0]),
                 np.log(np.linalg.det(doe_obj_nongb.results["FIM"])),
             ]
-            # print(float(doe_obj_nongb.results["Experiment Design"][0]))
-            # print(np.log(np.linalg.det(doe_obj_nongb.results["FIM"])))
-            print("\n\nStandard Model" + "~" * 20 + "\n\n")
-            doe_obj_nongb.model.pprint()
-            print("\n\nGREYBOX Model" + "~" * 20 + "\n\n")
-            doe_obj_gb.model.pprint()
-            print("\n\n")
-            # doe_obj_gb.model.obj_cons.egb_fim_block.pprint()
-            print("NON-GB FIM")
-            print(doe_obj_nongb.results["FIM"])
             optimal_objective_value = np.log(np.linalg.det(doe_obj_gb.results["FIM"]))
-            # Unfix the hour variable
-            # doe_obj_gb.model.scenario_blocks[0].hour.unfix()
-            # doe_obj_gb.model.scenario_blocks[0].hour.setlb(None)
-            # doe_obj_gb.model.scenario_blocks[0].hour.setub(None)
-            # KKT debugging
-            from pyomo.contrib.pynumero.interfaces.pyomo_grey_box_nlp import (
-                PyomoNLPWithGreyBoxBlocks,
-            )
 
-            doe_obj_gb.model._obj.deactivate()
-            nlp = PyomoNLPWithGreyBoxBlocks(doe_obj_gb.model)
-            nlp2 = doe_obj_gb.nlp
-
-            # Grab standard NLP
-            from pyomo.contrib.pynumero.examples.sqp import sqp, load_solution
-            from pyomo.contrib.pynumero.linalg.ma27_interface import MA27
-
-            linear_solver = MA27()
-            linear_solver.set_cntl(1, 1e-6)  # pivot tolerance
-            # sqp(nlp, linear_solver)
-            print(nlp.get_primals())
-            print(nlp.evaluate_jacobian().toarray())
-
-            from pyomo.contrib.pynumero.sparse import BlockVector, BlockMatrix
-
-            z = BlockVector(2)
-            z.set_block(0, nlp.get_primals())
-            z.set_block(1, nlp.get_duals())
-            z2 = BlockVector(2)
-            z2.set_block(0, nlp2.get_primals())
-            z2.set_block(1, nlp2.get_duals())
-            print("Z BLOCK - Primals")
-            print(z.get_block(0))
-            print(z2.get_block(0))
-            print("Z BLOCK - Duals")
-            print(z.get_block(1))
-            print(z2.get_block(1))
-            print("GRAD OBJECTIVE")
-            print(nlp.evaluate_grad_objective())
-            print(nlp2.evaluate_grad_objective())
-            print("GRAD JACOBIAN WITH Z BLOCK")
-            print(nlp.evaluate_jacobian().transpose() * z.get_block(1))
-            print(nlp2.evaluate_jacobian().transpose() * z2.get_block(1))
-            grad_lag = (
-                nlp.evaluate_grad_objective()
-                + nlp.evaluate_jacobian().transpose() * z.get_block(1)
-            )
-            grad_lag2 = (
-                nlp2.evaluate_grad_objective()
-                + nlp2.evaluate_jacobian().transpose() * z2.get_block(1)
-            )
-            print("DUAL INFEASIBILITY")
-            print(grad_lag)
-            print(grad_lag2)
-            # print("INEQUALITY GRAD")
-            # print(nlp.evaluate_jacobian_ineq().toarray())
-            # from pyomo.contrib.pynumero.interfaces.pyomo_nlp import PyomoNLP
-            # nlp_nongb = PyomoNLP(doe_obj_nongb.model)
-            #
-            # # Some magic maybe
-            # from pyomo.contrib.pynumero.sparse import BlockVector, BlockMatrix
-            # z = BlockVector(2)
-            # z.set_block(0, nlp_nongb.get_primals())
-            # z.set_block(1, nlp_nongb.get_duals())
-            #
-            # grad_lag = (
-            #         nlp_nongb.evaluate_grad_objective()
-            #         + nlp_nongb.evaluate_jacobian().transpose() * z.get_block(1)
-            # )
-            #
-            # print(grad_lag)
-            # print(nlp_nongb.evaluate_grad_objective())
-            # print(nlp_nongb.evaluate_jacobian().transpose() * z.get_block(1))
-            #
-            # sqp(nlp_nongb, linear_solver)
         elif objective_option == "trace":
             optimal_objective_value = np.trace(np.linalg.inv(doe_obj_gb.results["FIM"]))
         elif objective_option == "minimum_eigenvalue":
@@ -235,14 +151,14 @@ def run_greybox_optimization():
 
     print(optimal_points)
 
-    # ax = rooney_biegler_sensitivity()
-    #
-    # ax[0, 0].plot(optimal_points[0][0], optimal_points[0][1], marker='*', color='gold', ms=20)
-    # ax[0, 0].plot(regular_doe[0], regular_doe[1], marker='*', color='blue', ms=20)
-    # ax[0, 1].plot(optimal_points[2][0], optimal_points[2][1], marker='*', color='gold', ms=20)
-    # ax[1, 0].plot(optimal_points[3][0], optimal_points[3][1], marker='*', color='gold', ms=20)
-    # ax[1, 1].plot(optimal_points[1][0], optimal_points[1][1], marker='*', color='gold', ms=20)
-    # plt.show()
+    ax = rooney_biegler_sensitivity()
+
+    ax[0, 0].plot(regular_doe[0], regular_doe[1], marker='o', fillstyle='none', markeredgewidth=3, color='blue', ms=15)
+    ax[0, 0].plot(optimal_points[0][0], optimal_points[0][1], marker='*', color='gold', ms=20)
+    ax[0, 1].plot(optimal_points[2][0], optimal_points[2][1], marker='*', color='gold', ms=20)
+    ax[1, 0].plot(optimal_points[3][0], optimal_points[3][1], marker='*', color='gold', ms=20)
+    ax[1, 1].plot(optimal_points[1][0], optimal_points[1][1], marker='*', color='gold', ms=20)
+    plt.show()
 
 
 if __name__ == "__main__":
