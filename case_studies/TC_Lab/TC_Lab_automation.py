da3425227736476d3ef48612c5be550e7a391bea
djalky <dlakes94@gmail.com>
2025-10-13 12:55:21 -0400

    Add TC Lab running function

--- case_studies/TC_Lab/TC_Lab_automation.py
+++ case_studies/TC_Lab/TC_Lab_automation.py
@@ -7,8 +7,75 @@ import pyomo.environ as pyo
 from TC_Lab_parameter_estimation import TC_Lab_parmest
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import json
 import sys
+import os
+
+import tclab
+from tclab import TCLab, clock, Historian, Plotter
+
+
+
+def test_TC_Lab_experiment():
+    # Visually test the device
+    lab = TCLab()
+    lab.LED(50)
+    lab.close()
+    
+    # Run diagnostics to ensure
+    # device is running properly
+    print("Version = ", tclab.__version__)
+    tclab.diagnose()
+    
+
+def run_TC_Lab_experiment(Q1=None, Q2=None, time_points=None, t_jump=0.2):
+    # Make sure inputs are the right lengths
+    if Q1 is None and Q2 is None:
+        raise ValueError("Either Q1 or Q1 and Q2 must be defined to run a TC Lab experiment in this format. Please provide a list of power percentage values from 0 to 100 in the same length as the time_periods input.")
+    
+    # Open an instance of TC Lab
+    with TCLab() as lab:
+        # Set up historian to track values
+        h = Historian(lab.sources)
+        
+        # Set heaters to inital values
+        lab.Q1(Q1[0])
+        if Q2 is not None:
+            lab.Q2(Q2[0])
+        print(f"Set Heater 1 to {lab.Q1():.1f} %")
+        print(f"Set Heater 2 to {lab.Q2():.1f} %")
+
+        # Hold an index for time_points
+        time_ind = 0
+        
+        # Adjust heater setting to the control setting
+        # Increment by 0.2 seconds to avoid missing any
+        # time points.
+        for t in clock(time_points[-1], 0.2):
+            print(f"{t:5.1f} sec:  T1 = {lab.T1:4.1f} C   T2 = {lab.T2:4.1f} C")
+            
+            if t > time_points[time_ind]:
+                # Set heater temps
+                lab.Q1(Q1[0])
+                if Q2 is not None:
+                    lab.Q2(Q2[0])
+                
+                # Increment time index
+                time_ind += 1
+                h.update(t)
+    
+    # Write the historian to a csv and
+    # return a pandas dataframe with
+    # the data.
+    h.to_csv("TCLab_temp_data.csv")
+    
+    tclab_df = pd.read_csv("TCLab_temp_data.csv")
+    
+    # Remove temp_file
+    os.remove("TCLab_temp_data.csv")
+    
+    return tclab_df
 
 
 def automated_TC_Lab_model_identification(initial_datasets=None, n=2):
@@ -126,7 +193,13 @@ if __name__ == "__main__":
     data_sets = []
     
     # Run automated DoE
-    theta, FIM = automated_TC_Lab_model_identification(initial_datasets=data_sets, n=2)
+    # theta, FIM = automated_TC_Lab_model_identification(initial_datasets=data_sets, n=2)
+    
+    Q1 = [100, 100, 100, 50, 50, 50, 0, 0, 0, 50, 50, 50,]
+    time_points = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, ]
+    data = run_TC_Lab_experiment(Q1=Q1, time_points=time_points)
+    
+    print(data)
     
     # Print and plot the theta values and posteriors?
     
